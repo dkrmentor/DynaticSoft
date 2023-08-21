d1e3a545ea7586ab5bf364808a9847df002a4446
dkrmentor <dkrmentor@gmail.com>
2023-08-22 03:45:05 +0500

    tabs

---
+++ inner/tab.js
@@ -0,0 +1,23 @@
+ 
+ document.addEventListener("DOMContentLoaded", function () {
+  const tabLinks = document.querySelectorAll(".tabbing-links li");
+  const tabContents = document.querySelectorAll(".tabs");
+
+  tabLinks.forEach((tabLink) => {
+    tabLink.addEventListener("click", () => {
+      // Remove current class from all tab links and contents
+      tabLinks.forEach((link) => link.classList.remove("current"));
+      tabContents.forEach((content) => content.classList.remove("current"));
+
+      // Add current class to the clicked tab link and corresponding content
+      const targetTab = tabLink.getAttribute("data-targetit");
+      document.querySelector(`.${targetTab}`).classList.add("current");
+      tabLink.classList.add("current");
+    });
+  });
+
+  // Activate the default tab
+  tabLinks[0].click();
+});
+
+
